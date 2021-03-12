0561d0832b71fcade8e29082b5e334300ff67348
Sing-Kai Chiu <singkai.chiu@gmail.com>
2021-03-12 10:13:34 +0000

    strtucting app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,5 +1,25 @@
 package main
 
+import(
+	"fmt"
+)
+
+// App - struct which contains things like pointers to database connections
+type App struct{
+
+}
+
+func (app *App) Run() error {
+	fmt.Println("Setting up App")
+	return nil
+}
+
 func main(){
 	fmt.Println("go rest api")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up REST API")
+		fmt.Println(err)
+	}
+
 }
