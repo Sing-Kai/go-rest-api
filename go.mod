6b95ab40e3259133ac50d08e6d312b5aec09d267
Sing-Kai Chiu <singkai.chiu@gmail.com>
2021-03-13 22:34:18 +0000

    added testing with resty

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.15
 
 require (
 	github.com/denisenkom/go-mssqldb v0.9.0 // indirect
+	github.com/go-resty/resty/v2 v2.5.0
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
 	github.com/jinzhu/now v1.1.1 // indirect
---
+++ test/health_test.go
@@ -0,0 +1,20 @@
+// +build e2e
+
+package test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/go-resty/resty/v2"
+)
+
+func TestHealthEndPoint(t *testing.T) {
+	fmt.Println("Running E2E test fro health check endpoint")
+	client := resty.New()
+	resp, err := client.R().Get("http://localhost:8080/api/health")
+	if err != nil {
+		t.Fail()
+	}
+	fmt.Println(resp.StatusCode())
+}
